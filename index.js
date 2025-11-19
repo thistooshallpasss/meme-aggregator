cfa0c721c5325161b9a8b0673e3aba0624cf9c51
thistooshallpasss <prayagrajvoice@gmail.com>
2025-11-20 02:33:19 +0530

    fix: monorepo detection for Railway

---
+++ index.js
@@ -0,0 +1,4 @@
+// This file is only for Railway to detect root as a Node project.
+// It should NOT run the server.
+// Railway will use railway.json to deploy backend + worker.
+console.log("Railway monorepo root loaded. Services defined in railway.json.");
